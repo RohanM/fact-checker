49e39753e93a2b7273a69cbf89aaffd875d0edf3
Rohan Mitchell <rohan@rohanmitchell.com>
2023-03-28 18:57:46 +1100

    Tweak prompts for consistent formatting

--- src/chains/prompts.ts
+++ src/chains/prompts.ts
@@ -8,7 +8,7 @@ Today we're going to fact-check an article.
 Article: {article}
 
 Note down statements or claims that seem to be factual, important to the overall argument,
-  or potentially controversial(as dotpoints):
+  or potentially controversial (as dotpoints):
 `,
 });
 
@@ -18,7 +18,7 @@ const componentsPrompt = new PromptTemplate({
 Claims: {claims}
 
 Rewrite this list of claims. As you do, if you notice one that is multifaceted or
-complicated, break it down into simpler, more manageable components that can be fact-checked individually.
+complicated, break it down into simpler, more manageable components that can be fact-checked individually. Write each claim as an individual dotpoint.
 `,
 });
 
@@ -30,7 +30,8 @@ Article: {article}
 Claims: {claims}
 
 Prioritise the above claims based on their importance to the article's overall argument,
-their potential impact, and the likelihood that they might be false or misleading.
+their potential impact, and the likelihood that they might be false or misleading. State
+only the prioritised claims (as dotpoints).
 `,
 })
 
