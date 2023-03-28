b82fca50514a497ff940bc88a477345ff358da56
Rohan Mitchell <rohan@rohanmitchell.com>
2023-03-28 14:13:48 +1100

    Add index.html

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import express from "express";
+import path from "path";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.static(path.join(__dirname, "../public")));
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
