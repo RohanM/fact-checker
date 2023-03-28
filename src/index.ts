41e9f34c31255f866f52d4195b3f1151eacc6db0
Rohan Mitchell <rohan@rohanmitchell.com>
2023-03-28 14:18:36 +1100

    Echo back submission

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,21 @@
 import express from "express";
+import bodyParser from "body-parser";
 import path from "path";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, "../public")));
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.use(bodyParser.json());
+
+app.post("/fact-check", (req, res) => {
+  const article = req.body.article;
+
+  if (article) {
+    res.json({ article: article });
+  } else {
+    res.status(400).json({ error: "Missing 'article' parameter in request body." });
+  }
 });
 
 app.listen(port, () => {
