85a1187b719bd3d45dea1e0c5b3c488b53f90155
Rohan Mitchell <rohan@rohanmitchell.com>
2023-03-28 14:47:41 +1100

    Perform basic model request

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
 import express from "express";
 import bodyParser from "body-parser";
 import path from "path";
+import { fileURLToPath } from "url";
+
+import { OpenAI } from "langchain";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -8,11 +14,14 @@ const port = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, "../public")));
 app.use(bodyParser.json());
 
-app.post("/fact-check", (req, res) => {
+app.post("/fact-check", async (req, res) => {
   const article = req.body.article;
 
   if (article) {
-    res.json({ article: article });
+    const model = new OpenAI({ temperature: 0, modelName: "gpt-3.5-turbo" });
+    const text = await model.call(article);
+
+    res.json({ text: text });
   } else {
     res.status(400).json({ error: "Missing 'article' parameter in request body." });
   }
